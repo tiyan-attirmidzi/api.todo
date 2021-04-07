c0b423645eba0798ef79cde0589101c72a832d37
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-04-07 15:38:53 +0800

    configure eslint typescript

--- src/controller/todos/index.ts
+++ src/controller/todos/index.ts
@@ -1,16 +1,15 @@
-import { error } from 'console'
 import { Request, Response } from 'express'
 
 import TodoModel from '../../models/todo'
 import { Todo } from '../../types/todo'
 
 
-export const getTodos = async (req: Request, res: Response) => {
+export const getTodos = async (req: Request, res: Response): Promise<void> => {
     const todos: Todo[] =  await TodoModel.find()
     res.status(200).json({ todos })
 }
 
-export const getTodo = async (req: Request, res: Response) => {
+export const getTodo = async (req: Request, res: Response): Promise<void> => {
     await TodoModel.findById(req.params.id, (err, result) => {
         if (err) {
             res.status(400).json({
